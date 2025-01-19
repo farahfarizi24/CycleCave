fd7d5342ec70993061a2fd6e602449414426e7b6
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2025-01-20 07:41:40 +1100

    Update City2

---
+++ Assets/Scripts/SetGameObjActive.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class SetGameObjActive : MonoBehaviour
+{
+    public CheckTrigger trigger;
+    public GameObject[] GOList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.TriggerHit)
+        {
+            if (GOList.Length > 0) {
+
+                for (int i = 0; i < GOList.Length; i++)
+                {
+                    GOList[i].gameObject.SetActive(true);
+                }
+              
+                
+                   Array.Clear(GOList, 0, GOList.Length);
+                
+            }
+           
+        }
+    }
+}
